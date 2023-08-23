32e6393a733ad342ce31f8a86b28c1abcbf0cbf1
Sunshine-Hyh <794002729@qq.com>
2023-08-23 21:41:15 +0800

    删除无关代码

--- muduo/build/release-cpp11/muduo/net/inspect/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/neo/chatserver/muduo/muduo-master/muduo/net/inspect
-# Build directory: /home/neo/chatserver/muduo/build/release-cpp11/muduo/net/inspect
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- muduo/build/release-cpp11/muduo/net/protobuf/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/neo/chatserver/muduo/muduo-master/muduo/net/protobuf
-# Build directory: /home/neo/chatserver/muduo/build/release-cpp11/muduo/net/protobuf
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
